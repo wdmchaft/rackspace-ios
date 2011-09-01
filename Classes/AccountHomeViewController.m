c29cc36a18dd70e41168ce0b5f9e4b95a651fa6c
Mike Mayo <greenisus@gmail.com>
2011-08-31 17:03:51 -0700

    code review changes

--- Classes/AccountHomeViewController.m
+++ Classes/AccountHomeViewController.m
@@ -130,7 +130,6 @@
     computeRow = (self.account.serversURL && [self.account.serversURL host]) ? totalRows++ : -1;
     storageRow = (self.account.filesURL && [self.account.filesURL host]) ? totalRows++ : -1;
     loadBalancingRow = [self.account loadBalancerURLs] ? totalRows++ : -1;
-    //loadBalancingRow = -1;
 
     if (self.account.provider.rssFeeds && [self.account.provider.rssFeeds count] > 0) {
         rssFeedsRow = totalRows++;
